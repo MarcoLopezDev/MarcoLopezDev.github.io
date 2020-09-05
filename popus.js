4ba36d74ea8192eb595166a28f00c0accaef3626
Marco Lopez <marcodevlg@gmail.com>
2020-09-05 17:16:18 -0400

    Ingresnado segundo carrousel + 1 peliculas

--- popus.js
+++ popus.js
@@ -12,3 +12,20 @@ btnCerrarPopup.addEventListener('click', function(){
     overlay.classList.remove('active');
     popup.classList.remove('active')
 });
+
+
+/*Mother*/
+var btnAbrirPopup11 = document.getElementById('play11'),
+    overlay11 = document.getElementById('overlay11'),
+    popup11 = document.getElementById('popup11'),
+    btnCerrarPopup11 = document.getElementById('btn-cerrar-popup11');
+
+btnAbrirPopup11.addEventListener('click', function(){
+    overlay11.classList.add('active');
+    popup11.classList.add('active')
+});
+
+btnCerrarPopup11.addEventListener('click', function(){
+    overlay11.classList.remove('active');
+    popup11.classList.remove('active')
+});
